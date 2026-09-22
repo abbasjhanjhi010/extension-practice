08eec6563b375b328d714bf2e7579caa353ebb4e
Abbas <MuhammadAbbas1011@outlook.com>
2026-09-23 00:31:21 +0800

    added logo and index.html

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+/*
+chrome.action.onClicked.addListener ( tab => {
+    chrome.scripting.executeScript({
+        target: { tabId:  tab.id},
+        func: () => {
+            alert("Hello world from my universe!")
+        }
+    });
+});
+
+*/
